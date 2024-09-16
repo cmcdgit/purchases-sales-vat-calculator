456deaf25bf1070c57d4f70a9af2cd218fb42e9c
Ciaran Mc Devitt <cmcdgit@gmail.com>
2024-09-16 21:38:20 +0000

    APIs set up and access to sheets confirmed

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive",
+    "https://www.googleapis.com/auth/spreadsheets"
+    ]
+
+CREDS = Credentials.from_service_account_file('credentials.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+PURCHASES_SHEET = GSPREAD_CLIENT.open('vat_purchases')
+SALES_SHEET = GSPREAD_CLIENT.open('vat_sales')
+
+purchases = PURCHASES_SHEET.worksheet("January")
+sales = SALES_SHEET.worksheet("January")
+
+purchases_data = purchases.get_all_values()
+print("\npurchases_data:")
+print(purchases_data)
+
+sales_data = sales.get_all_values()
+print("\nsales_data:")
+print(sales_data)
