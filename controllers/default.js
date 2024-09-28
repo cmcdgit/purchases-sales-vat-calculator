383b00af94b1881deeb10eecd893967433c34a37
Ciaran Mcdevitt <cmcdgit@gmail.com>
2024-09-28 21:48:07 +0000

    Adjust terminal size to 190 cols

--- controllers/default.js
+++ controllers/default.js
@@ -18,7 +18,7 @@ function socket() {
         // Spawn terminal
         client.tty = Pty.spawn('python3', ['run.py'], {
             name: 'xterm-color',
-            cols: 140,
+            cols: 190,
             rows: 64,
             cwd: process.env.PWD,
             env: process.env
